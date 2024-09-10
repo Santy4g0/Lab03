455c0f0a3b290d6f04fcbf3ccbc384c8edbf3a0c
Santy4g0 <santgruiz853@gmail.com>
2024-09-10 15:47:45 -0500

    Update Cronometro.java
    Agregamos parte de la alarma para el cronometro

--- Cronometro/src/main/java/com/mycompany/cronometro/Cronometro.java
+++ Cronometro/src/main/java/com/mycompany/cronometro/Cronometro.java
@@ -14,13 +14,22 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class Cronometro {
-    private final Timer timer;
-    private final SimpleDateFormat sdf;
+    private Timer timer;
+    private Timer alarmTimer;
+    private SimpleDateFormat sdf;
+    private long alarmTimeMinutes;
+    private long alarmIntervalSeconds;
+    private boolean alarmTriggered;
 
-    public Cronometro() {
+    public Cronometro(long alarmTimeMinutes, long alarmIntervalSeconds) {
+        this.alarmTimeMinutes = alarmTimeMinutes;
+        this.alarmIntervalSeconds = alarmIntervalSeconds;
         timer = new Timer(true);
+        alarmTimer = new Timer(true);
         sdf = new SimpleDateFormat("HH:mm:ss");
+        alarmTriggered = false;
         startTimer();
+        startAlarm(); // Estructura básica para la alarma
     }
 
     private void startTimer() {
@@ -29,10 +38,33 @@ public class Cronometro {
             public void run() {
                 System.out.println(sdf.format(new Date()));
             }
-        }, 0, 1000);
+        }, 0, 1000); // Actualiza cada segundo
+    }
+
+    private void startAlarm() {
+        // Estructura básica para la alarma
+        TimerTask alarmTask = new TimerTask() {
+            @Override
+            public void run() {
+                if (!alarmTriggered) {
+                    alarmTriggered = true;
+                    System.out.println("¡Alarma activada!");
+                }
+                System.out.println("¡Ding!");
+            }
+        };
+
+        // Este temporizador activará la alarma después de cierto tiempo
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                alarmTimer.scheduleAtFixedRate(alarmTask, 0, alarmIntervalSeconds * 1000);
+            }
+        }, alarmTimeMinutes * 60 * 1000); // Convertimos minutos a milisegundos
     }
 
     public static void main(String[] args) {
-        Cronometro cronometro = new Cronometro();
+        // Configura la alarma para que se active después de 2 minutos y la alarma se repita cada 10 segundos
+        new Cronometro(2, 10);
     }
 }
