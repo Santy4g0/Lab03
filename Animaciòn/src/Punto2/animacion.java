95ea765d24ea8e275de63b06c6323d2c1e25ae6f
Santy4g0 <santgruiz853@gmail.com>
2024-09-24 10:30:14 -0500

    Add files via upload
    Se reemplazo por la carpeta correcta, corregimos detalles correspondientes al codigo fuente y se redistribuyeron los botones en la interfaz grafica para mejor organizaciòn y màs comodidad para el usuario.

---
+++ Animaciòn/src/Punto2/animacion.java
@@ -0,0 +1,15 @@
+
+package Punto2;
+/**
+ *
+ * @author Elkin Santiago Ruiz Rodriguez, Juan David Ruiz Gomez.
+ */
+public class animacion {
+    
+
+           
+    public static void main(String[] args) {
+        
+    }
+    
+}
